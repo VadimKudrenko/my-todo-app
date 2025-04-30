4eadba063814587ad16bf0cc8f964b42ef518ad7
VadimKudrenko <48049362+VadimKudrenko@users.noreply.github.com>
2025-04-30 14:37:08 +0300

    final version + update README

--- README.md
+++ README.md
@@ -1,9 +1,80 @@
-# React TypeScript Starter Pack
+# 📝 Todo App
 
-To use this template click `Use this template`
+A simple yet functional Todo application built with React and TypeScript.
+This project demonstrates working with an external API.
+Users can create, view, complete, delete, and filter todos based on their status.
 
-### Available Scripts
+## 🔗 Live Preview
 
-`Deploy` - available to deploy your application to gh-pages, to deploy the project, change the second line of code in package.json, specifically the homepage value, from "." to the repository name
+[Check the demo here](https://vadimkudrenko.github.io/my-todo-app)
 
-`SCSS Preprocessor` - available to write your styles with modern style language
+## 🛠️ Technologies Used
+
+### Core
+- **React** (v18+) – UI library
+- **TypeScript** (v5+) – Type safety
+- **SCSS** – Component-based styling
+
+### Data Handling
+- **External REST API** – Used for fetching, updating, and deleting todos
+- **Axios** – For performing HTTP requests
+
+### UI/UX
+- **Transition Group** – It is used to implement smooth animations during any interactions with todos.
+
+### Development & Deployment
+- **Vite** – Build tool
+- **ESLint** – Code quality & linting
+- **GitHub Pages** – Hosting and deployment
+
+## ✨ Features
+
+- View all previously created todos pulled from an API
+- Add new todos using an input field
+- Mark todos as completed using checkboxes
+- **Sorting**: Filtertodos by their status: All / Active / Completed
+- Delete completed todos with a single click
+- Real-time UI updates based on server-side data
+- Minimalistic and responsive design
+
+
+## ✨ Features
+
+- **Classic Todo**:  Users can add new tasks, edit existing ones, and remove any todo items at any time.
+
+- **Filter todos by status**: Filter the list to show all tasks, only completed, or only active todos.
+
+- **Server-side persistence**: All todos are stored on a remote server and persist even after a page reload.
+
+- **Toggle completion status**: Each todo has a status that allows users to mark it as completed or return it to active.
+
+- **Real-time UI updates**: The interface immediately reflects changes like status toggles, additions, or deletions based on API responses.
+
+- **Minimalistic and pleasant design**: The layout responds to every user action with smooth animations that improves the user experience.
+
+
+## 🚀 Getting Started
+
+Follow these instructions to run the project locally:
+
+### 1. Clone the repository
+```bash
+git clone https://github.com/VadimKudrenko/my-todo-app.git
+```
+```bash
+cd my-todo-app
+```
+
+### 2.Install dependencies:
+```bash
+npm install
+# or
+yarn install
+```
+
+### 3.Run the project locally:
+```bash
+npm start
+# or
+yarn start
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,96 @@
-import React from 'react';
-import './App.scss';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+import { getTodos, USER_ID } from './api/todos';
+import { Todo } from './types/Todo';
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { UserWarning } from './UserWarning';
+import { TodoList } from './components/TodoList';
+import { TodoForm } from './components/TodoForm';
+import { ErrorNotification } from './components/ErrorNotification';
 
 export const App: React.FC = () => {
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [loadingTodos, setLoadingTodos] = useState<Set<number>>(new Set());
+
+  const errorTimerId = useRef(0);
+
+  const addNewTodo = (newTodo: Todo) => {
+    setTodoList(currentList => [...currentList, newTodo]);
+  };
+
+  const removeTodo = (id: number) => {
+    setTodoList(currentList => currentList.filter(todo => todo.id !== id));
+  };
+
+  const showErrorMessage = (message: string, delay = 3000) => {
+    clearInterval(errorTimerId.current);
+    setErrorMessage(message);
+    errorTimerId.current = window.setTimeout(() => setErrorMessage(''), delay);
+  };
+
+  const handleErrorRemove = () => {
+    setErrorMessage('');
+  };
+
+  const toggleTodoStatus = useCallback((id: number) => {
+    setTodoList(currentList =>
+      currentList.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    );
+  }, []);
+
+  const updateTodoTitle = useCallback((id: number, updatedTitle: string) => {
+    setTodoList(currentList =>
+      currentList.map(todo =>
+        todo.id === id ? { ...todo, title: updatedTitle } : todo,
+      ),
+    );
+  }, []);
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodoList)
+      .catch(() => showErrorMessage('Unable to load todos'));
+  }, []);
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <TodoForm
+          todoList={todoList}
+          setLoadingTodos={setLoadingTodos}
+          addNewTodo={addNewTodo}
+          setTempTodo={setTempTodo}
+          showErrorMessage={showErrorMessage}
+          toggleTodoStatus={toggleTodoStatus}
+        />
+        {todoList && (
+          <TodoList
+            todoList={todoList}
+            tempTodo={tempTodo}
+            loadingTodos={loadingTodos}
+            removeTodo={removeTodo}
+            updateTodoTitle={updateTodoTitle}
+            showErrorMessage={showErrorMessage}
+            toggleTodoStatus={toggleTodoStatus}
+            setLoadingTodos={setLoadingTodos}
+          />
+        )}
+      </div>
+
+      <ErrorNotification
+        errorMessage={errorMessage}
+        handleErrorRemove={handleErrorRemove}
+      />
     </div>
   );
 };
