b9e58eb5d8a69c08af4109e15b924a363e5502c1
VadimKudrenko <48049362+VadimKudrenko@users.noreply.github.com>
2025-05-06 18:01:01 +0300

    changed the main headings

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ export const App: React.FC = () => {
 
   return (
     <div className="todoapp">
-      <h1 className="todoapp__title">todos</h1>
+      <h1 className="todoapp__title">Plans</h1>
 
       <div className="todoapp__content">
         <TodoForm
